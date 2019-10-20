f4af2fdc529764ffdd0e97d19bd1353a8db5846d
tianyawy <tianya_wy@hotmail.com>
2019-10-20 16:27:05 +0800

    [victoryw] #N/A refactor: remove unused purchase order case

--- api/src/main/java/com/victory/ddd/china/sample/api/JerseyConfig.java
+++ api/src/main/java/com/victory/ddd/china/sample/api/JerseyConfig.java
@@ -1,6 +1,5 @@
 package com.victory.ddd.china.sample.api;
 
-import com.victory.ddd.china.sample.api.controller.PurchaseOrderResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
